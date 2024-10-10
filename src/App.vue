c0678712930c230cb6f0dc875e9dbf55a0a07037
Saravana Thiyagarajan <sthiyagarajan@uplandsoftware.com>
2024-10-10 21:41:44 +0530

    Basic setup

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  // Properties returned from data() become reactive state
+  // and will be exposed on `this`.
+  data() {
+    return {
+      count: 0,
+    };
+  },
+
+  // Methods are functions that mutate state and trigger updates.
+  // They can be bound as event handlers in templates.
+  methods: {
+   
+  },
+
+  // Lifecycle hooks are called at different stages
+  // of a component's lifecycle.
+  // This function will be called when the component is mounted.
+  mounted() {
+
+  },
+};
+</script>
+
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
+
+<style >
+
+</style>
